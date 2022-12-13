9f1d6ef53e3402f90542d25d73e13fc6bf01b1ed
Taha Rhidouani <37096708+TahaInc@users.noreply.github.com>
2022-12-12 21:16:48 -0500

    updated page urls

--- server.js
+++ server.js
@@ -47,14 +47,14 @@ app.get("/order", (req, res) => {
   if (req.loggedIn) {
     res.render("orderMenu.ejs", { user: req.user });
   } else {
-    res.redirect("./login");
+    res.redirect("/login");
   }
 });
 
 app.get("/login", (req, res) => {
   // Serve the homepage if already logged in
   if (req.loggedIn) {
-    res.redirect("./");
+    res.redirect("/");
   } else {
     res.render("login.ejs");
   }
@@ -80,7 +80,7 @@ app.post("/login", (req, res) => {
 app.get("/register", (req, res) => {
   // Serve the homepage if already logged in
   if (req.loggedIn) {
-    res.redirect("./");
+    res.redirect("/");
   } else {
     res.render("register.ejs");
   }
@@ -106,7 +106,7 @@ app.get("/logout", (req, res) => {
   // Set the credential cookies to null and redirect to home page
   req.session.username = null;
   req.session.password = null;
-  res.redirect("./");
+  res.redirect("/");
 });
 
 app.get("/restaurants", (req, res) => {
